c7a856fa4ccb4a98c5684cc813428fea252d00d6
Brandon Liu <bliu88@live.com>
2019-02-09 22:58:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,14 @@
 # Your code goes here!
 class Dog 
-  def  intialize(name, bark)
-    @name = name 
-  end 
-  
   def name 
     @name 
   end
   
-  def name=(new_name)
-    @name = new_name
+  def name=(dogs_name)
+    @name = dogs_name
   end
   
-  def bark
-    @bark 
-  end
-  
-  def bark=(woof)
-    @bark = ("woof!")
+  def bark 
+    puts "woof!"
   end
 end
